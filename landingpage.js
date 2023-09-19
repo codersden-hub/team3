cd8d2754a6018967177c3f1ef7591024c8ff95a1
Leonardo <112240030+Watch30@users.noreply.github.com>
2023-09-18 21:17:34 -0300

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -6,6 +6,12 @@ const general = document.querySelector('.general-class')
 const sports = document.querySelector('.sport-class')
 const history = document.querySelector('.history-class')
 const computer = document.querySelector('.computer-class')
+const b_general = document.querySelector('.play_general')
+const b_science = document.querySelector('.play_travel')
+const b_music = document.querySelector('.play_music')
+const b_sports = document.querySelector('.play_sport')
+const b_history = document.querySelector('.play_history')
+const b_computer = document.querySelector('.play_computer')
 
 
 //Displaying welcome user on landing page 
@@ -35,7 +41,6 @@ general.addEventListener('click', function(){
     
 })
 
-
 sports.addEventListener('click', function(){
     window.location.href = "./sports/sportsmodal.html";
 })
@@ -49,4 +54,26 @@ computer.addEventListener('click', function(){
     window.location.href = "./computer/computermodal.html";
 })
 
+b_general.addEventListener('click', function(){
+    window.location.href = "./general/generalmodal.html";  
+})
 
+b_sports.addEventListener('click', function(){
+    window.location.href = "./sports/sportsmodal.html";
+})
+
+b_science.addEventListener('click', function(){
+    window.location.href = "./science/sciencemodal.html";  
+})
+
+b_computer.addEventListener('click', function(){
+    window.location.href = "./computer/computermodal.html";  
+})
+
+b_history.addEventListener('click', function(){
+    window.location.href = "./history/historymodal.html";  
+})
+
+b_music.addEventListener('click', function(){
+    window.location.href = "./music/musicmodal.html";  
+})
